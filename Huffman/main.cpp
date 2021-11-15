0301d9fa99fe4f68c4d8d65de6d9582c162b7b6c
“gaoyuqi” <gaoyuqi@toki.waseda.jp>
2021-11-15 14:44:43 +0800

    Initial Commit

---
+++ Huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Huffman
+//
+//  Created by 高裕淇 on 2021/11/15.
+//  Copyright © 2021 高裕淇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
